edb19e8e6baf920447e99300799230b9e0f6ef21
Simone Bembi <simone.bembi@gmail.com>
2021-02-17 19:25:59 +0100

    Added automatic scroll stop while manually scrolling

--- client/components/manager/session/session.tsx
+++ client/components/manager/session/session.tsx
@@ -4,7 +4,7 @@ import { ISession, ISessionLog, SessionLogType, SessionStatus } from '@/state/mo
 import { values } from 'mobx';
 import { observer } from 'mobx-react-lite';
 import { string } from 'mobx-state-tree/dist/internal';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import dayjs from 'dayjs';
 
@@ -22,35 +22,78 @@ const colorsByLogType: {
     [SessionLogType.STDERR]: "#d08770"
 }
 
-export const SessionLogs = observer((props: { logs: ISessionLog[], onLogsProportionChanged: (proportions: number) => void }) => {
+const useScroll = (onLogsProportionChanged: (proportions: number) => void, deps: any[]) => {
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const [scrolling, setScrolling] = useState(false);
+    const [scrollTop, setScrollTop] = useState(0);
+    const timeoutIdRef = useRef<NodeJS.Timeout>();
+    const [scrollProportions, setScrollProportions] = useState(0);
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
-    const logsContainerDiv = useRef<HTMLDivElement | null>(null);
+    useLayoutEffect(() => {
+        const onResize = () => {
+            setWindowHeight(window.innerHeight);
+        };
+        document.addEventListener('resize', onResize);
+        return () => document.removeEventListener('resize', onResize);
+    }, []);
 
     useEffect(() => {
-        const container = logsContainerDiv.current;
-        if (container) {
+        const container = containerRef.current;
+        if (container && !scrolling) {
             container.scrollTop = container.scrollHeight;
             const clientHeight = container.clientHeight;
             const scrollHeight = container.scrollHeight;
             if (scrollHeight > clientHeight) {
                 // Full height: 100%
-                props.onLogsProportionChanged(1);
+                setScrollProportions(1)
+                onLogsProportionChanged(1);
             } else {
                 let contentHeight = 0;
                 for (const child of container.children) {
                     contentHeight += child.clientHeight;
                 }
                 const proportion = contentHeight / clientHeight;
-                props.onLogsProportionChanged(proportion);
+                setScrollProportions(proportion);
+                onLogsProportionChanged(proportion);
             }
         }
-    }, [logsContainerDiv.current, props.logs.length]);
+    }, [containerRef.current, windowHeight, ...deps]);
+
+    useEffect(() => {
+        return () => clearTimeout(timeoutIdRef.current);
+    }, [])
+
+    const onScroll = () => {
+        const newScrollTop = containerRef.current.scrollTop;
+        if (scrollTop > newScrollTop || scrolling) {
+            setScrolling(true);
+            onLogsProportionChanged(0);
+            clearTimeout(timeoutIdRef.current)
+            timeoutIdRef.current = setTimeout(() => {
+                setScrolling(false);
+                setScrollProportions(scrollProportions);
+                onLogsProportionChanged(scrollProportions);
+            }, 5000);
+        }
+
+        setScrollTop(containerRef.current.scrollTop);
+    }
+
+    return { containerRef, onScroll };
+}
+
+export const SessionLogs = observer((props: { logs: ISessionLog[], onLogsProportionChanged: (proportions: number) => void }) => {
+
+    const { containerRef, onScroll } = useScroll(props.onLogsProportionChanged, [props.logs.length]);
 
     if (!props.logs) return null;
 
     return <div
-        ref={logsContainerDiv}
-        className="m-2 text-nord-3 dark:text-nord4 py-5 rounded-md flex-grow mt-12 mb-36 flex flex-col min-w-0 min-h-0 overflow-x-hidden no-horizontal-scrollbar">
+        ref={containerRef}
+        className="m-2 text-nord-3 dark:text-nord4 py-5 rounded-md flex-grow mt-12 mb-36 flex flex-col min-w-0 min-h-0 overflow-x-hidden no-horizontal-scrollbar"
+        style={{ scrollBehavior: 'smooth' }}
+        onScroll={onScroll}>
         {props.logs.map((log: ISessionLog, key) => {
             const color = colorsByLogType[log.type];
             return <p className="mx-10 leading-relaxed text-sm whitespace-nowrap max-w-full min-w-0 flex items-center" key={key}>
