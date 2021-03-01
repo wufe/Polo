d7278cf131435df9cf926d378ea703f0a26ba371
Simone Bembi <simone.bembi@gmail.com>
2021-03-01 20:17:12 +0100

    Change: killing replacement session too if the user stops the to-be-replaced session

--- pkg/background/session-clean-worker.go
+++ pkg/background/session-clean-worker.go
@@ -72,11 +72,14 @@ func (w *SessionCleanWorker) startAcceptingSessionCleanRequests() {
 			if session.GetKillReason() == models.KillReasonStopped {
 				// FEATURE: Hot swap
 				// Check if the killed session should have been replaced by another session
-				for _, s := range w.sessionStorage.GetAllAliveSessions() {
-					// If so, tell this session that it is not a replacement anymore
-					if s.Replaces() == session {
-						s.IsReplacementFor(nil)
-						w.sessionStorage.Update(s)
+				for _, replacement := range w.sessionStorage.GetAllAliveSessions() {
+					if replacement.Replaces() == session {
+						// If so, tell this session that it is not a replacement anymore
+						replacement.IsReplacementFor(nil)
+						// And destroy it too
+						replacement.SetKillReason(models.KillReasonStopped)
+						w.mediator.DestroySession.Enqueue(replacement, nil)
+						w.sessionStorage.Update(replacement)
 					}
 				}
 			}
