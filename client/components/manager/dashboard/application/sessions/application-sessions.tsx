5ab517388988e82ecc3e391d3c180ae278c0e88f
Simone Bembi <simone.bembi@gmail.com>
2021-03-29 00:26:31 +0200

    Enabled sessions filter input

--- client/components/manager/dashboard/application/sessions/application-sessions.tsx
+++ client/components/manager/dashboard/application/sessions/application-sessions.tsx
@@ -1,6 +1,6 @@
 import { ISession } from '@/state/models';
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { useState } from 'react';
 import { ApplicationSession } from '../session/application-session';
 import './application-sessions.scss';
 
@@ -9,15 +9,25 @@ type TProps = {
 }
 
 export const ApplicationSessions = observer((props: TProps) => {
-    const visibleSessions = props.sessions
+
+    const [sessionsFilter, setSessionsFilter] = useState('');
+
+    let visibleSessions = props.sessions
         .filter(session => !session.replacesSession);
+
+    if (sessionsFilter) {
+        visibleSessions = visibleSessions
+            .filter(session => session.checkout.toLowerCase().indexOf(sessionsFilter.toLowerCase()) > -1);
+    }
 
     return <div className="flex flex-col items-stretch">
         <input
             type="text"
             placeholder="Filter sessions"
+            onChange={e => setSessionsFilter(e.target.value)}
+            value={sessionsFilter}
             className="bg-transparent border border-gray-200 dark:border-gray-500 text-sm py-2 px-3 rounded-md mb-1 outline-none" />
-        {visibleSessions.length && <span className="text-xs lg:text-sm text-gray-500 pl-2">{visibleSessions.length} sessions</span>}
+        {visibleSessions.length > 0 && <span className="text-xs lg:text-sm text-gray-500 pl-2">{visibleSessions.length} sessions</span>}
         <div className="mt-3">
             {visibleSessions
                 .map((session, key) => <ApplicationSession session={session} key={key} />)}
