a47621560094827b85684854c84fe91d17393a59
Simone Bembi <simone.bembi@gmail.com>
2021-03-29 13:26:31 +0200

    Fixed modal scroll
    Fixed session ready icon

--- client/components/manager/dashboard/application/session/application-session.tsx
+++ client/components/manager/dashboard/application/session/application-session.tsx
@@ -58,7 +58,7 @@ export const ApplicationSession = observer((props: { session: ISession }) => {
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 20 20"
                         fill={colorByStatus(props.session.status)}
-                        className="w-6">
+                        className="w-6 h-6 mt-0.5">
                         <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                     </svg>}
                 {(props.session.status === SessionStatus.STARTING ||
