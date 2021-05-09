cc7c7652e4c6e5101c11fc925f70d7d10d87404c
Simone Bembi <simone.bembi@gmail.com>
2021-05-09 21:03:45 +0200

    Notifications mobile fit

--- client/components/manager/notifications/notifications-container.tsx
+++ client/components/manager/notifications/notifications-container.tsx
@@ -6,18 +6,10 @@ type TProps = {
     notifications: INotification[];
 };
 export const NotificationsContainer = (props: React.PropsWithChildren<TProps>) => {
-    return <div className="fixed right-6 bottom-10 z-50 text-nord0 dark:text-nord4">
+    return <div className="fixed right-6 left-6 bottom-10 sm:left-auto z-50 text-nord0 dark:text-nord4">
         {props.notifications.map((notification, index) =>
             <Notification
                 key={notification.uuid}
                 notification={notification} />)}
-        {/* <Notification
-            danger>
-            There have been errors creating your session
-        </Notification>
-        <Notification
-            success>
-            Your session has been created
-        </Notification> */}
     </div>;
 }
