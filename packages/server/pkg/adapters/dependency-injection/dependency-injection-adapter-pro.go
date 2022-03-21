9df38c7a16185e3f9443fd2f3ae7ef0c83803377
Simone Bembi <simone.bembi@gmail.com>
2022-03-21 20:33:12 +0100

    style(server): printing pro build

--- packages/server/pkg/adapters/dependency-injection/dependency-injection-adapter-pro.go
+++ packages/server/pkg/adapters/dependency-injection/dependency-injection-adapter-pro.go
@@ -1,14 +1,25 @@
+//go:build pro
 // +build pro
 
 package dependency_injection_adapter
 
 import (
+	"github.com/wufe/polo/pkg/logging"
 	"go.uber.org/dig"
 
 	dependency_injection "github.com/wufe/polo/third_party/polo-pro/pkg/dependency-injection"
 )
 
 func Register(di *dig.Container) {
+	var logger logging.Logger
+
+	di.Invoke(func(l logging.Logger) {
+		logger = l
+	})
+
+	logger.Infoln("Pro build")
+	logger.Debugln("Injecting pro features")
+
 	dependency_injection.RegisterProvide(func(constructor interface{}) error {
 		return di.Provide(constructor)
 	})
