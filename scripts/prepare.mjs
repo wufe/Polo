748d301ad7be6af4c36688b6099167a20c8962f0
Simone Bembi <simone.bembi@gmail.com>
2022-03-26 14:57:22 +0100

    chore: removed go.mod creation from post install

--- scripts/prepare.mjs
+++ scripts/prepare.mjs
@@ -4,12 +4,3 @@ import 'zx/globals';
 
 // Installing Husky's hooks
 await $`husky install`;
-
-const cwd = process.cwd();
-const poloProPath = path.resolve(cwd, 'packages/server/third_party/polo-pro');
-const poloProGoModPath = path.join(poloProPath, 'go.mod');
-
-if (!fs.existsSync(poloProGoModPath)) {
-    await $`mkdir -p ${poloProPath}`;
-    await $`echo "module github.com/wufe/polo-pro\n\ngo 1.15\n" > ${path.join(poloProPath, 'go.mod')}`;
-}
